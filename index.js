ce26a1d025e546f5a5d06fccfb1ddee4fd1a818f
OctoKing-dev <51433961+OctoKing-dev@users.noreply.github.com>
2023-05-08 10:06:58 -0400

    Apply formatting and style fixes to index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const newGridButton = document.getElementById("newGridButton");
-const squareContainer = document.getElementById("squareContainer");
+const newGridButton = document.getElementById('newGridButton');
+const squareContainer = document.getElementById('squareContainer');
 
 // Create a square grid of size x size.
 function createNewGrid(size) {
   if (!Number.isInteger(size)) return;
   squareContainer.replaceChildren();
-  
-  const squareEle = document.createElement('div');
-  squareEle.className = "square";
 
-  for (let row=1; row <= size; row++) {
+  const squareEle = document.createElement('div');
+  squareEle.className = 'square';
+
+  for (let row = 1; row <= size; row++) {
     const rowEle = document.createElement('div');
-    rowEle.className = "square-row";
-    for (let column=1; column <= size; column++) {
+    rowEle.className = 'square-row';
+    for (let column = 1; column <= size; column++) {
       const newSquare = squareEle.cloneNode();
       rowEle.appendChild(newSquare);
     }

41cad0e93b6e9be80b94083196b680862d11d18b
OctoKing-dev <51433961+OctoKing-dev@users.noreply.github.com>
2023-05-08 10:29:41 -0400

    Give square random color once when colored

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const newGridButton = document.getElementById('newGridButton');
 const squareContainer = document.getElementById('squareContainer');
 
+function squareHovered() {
+  // Generate a random color if this is the first time it's been selected.
+  if (!this.style.backgroundColor) this.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+  let r = Math.floor(Math.random() * 256),
+      g = Math.floor(Math.random() * 256),
+      b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
 // Create a square grid of size x size.
 function createNewGrid(size) {
   if (!Number.isInteger(size)) return;
@@ -12,10 +24,13 @@ function createNewGrid(size) {
   for (let row = 1; row <= size; row++) {
     const rowEle = document.createElement('div');
     rowEle.className = 'square-row';
+
     for (let column = 1; column <= size; column++) {
       const newSquare = squareEle.cloneNode();
+      newSquare.addEventListener('mouseenter', squareHovered);
       rowEle.appendChild(newSquare);
     }
+
     squareContainer.appendChild(rowEle);
   }
 }
@@ -26,7 +41,7 @@ function promptNewGrid() {
     newGridSize = Number.parseInt(prompt("Please enter new grid size: (1 - 100)"));
     if (Number.isNaN(newGridSize) || newGridSize < 1 || newGridSize > 100) newGridSize = null;
   } while (!newGridSize);
-  
+
   createNewGrid(newGridSize);
 }
 
